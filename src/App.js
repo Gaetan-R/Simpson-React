58406bc13b31d88eff02cd6e0eb9fa22cb10c32b
Gaetan-r <richer.gaetan33@gmail.com>
2021-07-16 17:11:44 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import QuoteCard from './components/QuoteCards';
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar />
+      <QuoteCard />
     </div>
   );
 }
